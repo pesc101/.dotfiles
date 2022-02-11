5763ab166aea9e2707447e6eb6fad0884d1b6f40
Jan Strich <53601317+pesc101@users.noreply.github.com>
2022-02-11 12:41:15 +0100

    add exa options

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -10,11 +10,16 @@ set -g theme_hostname always
 # aliases
 alias g git
 command -qv nvim && alias vim nvim
+alias cdd cd
+alias cc cd
+alias dc cd
 
 
 if type -q exa
-  alias ls "exa -l -g --icons"
-  alias ll "exa -l -g --icons"
+  alias ls "exa -l -g --icons -h --time=changed --time-style=long-iso --git --sort=mod"
+  alias tree "exa -l --icons -T -L 2 -h --time=changed --time-style=long-iso --git --sort=mod"
+  alias tree2 "exa -l --icons -T -L 3 -h --time=changed --time-style=long-iso --git --sort=mod"
+  alias ll "exa -l -g --icons -h --time=changed --time-style=long-iso --git --sort=mod"
   alias lla "ll -a"
 end
 
